d54f376807d27c543260c93dd5eb5a9ded92e39c
DeeC-13 <d.couch430@gmail.com>
2024-11-12 13:08:59 +0000

    Add files via upload

---
+++ VanillaJS/JavaScript/jscode.js
@@ -0,0 +1,19 @@
+console.log('* Activity 1 *'); //Gives this section a title
+
+let a = (-5+8*6); //Calculates the numbers
+console.log(a); //Prints the andswer to the console
+
+let b = ((55+9)%9);
+console.log(b);
+
+let c = (20+-3*5/8);
+console.log(c);
+
+let d = (5+15/3*2-8%3);
+console.log(d);
+
+console.log('* Activity 2 *'); //Gives this section a title
+
+const aNumber = Number(window.prompt("Type a number", "")); //Asks the user to input a number
+const bNumber = Number(window.prompt("Type a number", ""));
+console.log(" The division of the first number and the second number is " +  (aNumber/bNumber)); // Prints the sentence then answer in the console
