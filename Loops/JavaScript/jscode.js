6df5603b08c893e6c165332b93317aa46936f508
DeeC-13 <d.couch430@gmail.com>
2024-11-13 12:09:12 +0000

    Add files via upload

---
+++ Loops/JavaScript/jscode.js
@@ -0,0 +1,16 @@
+console.log('*Activity 1*'); //Gives this section a title
+
+for (let n = 0; n < 15; n++){  //Increments by 1
+    if ((n % 2) == 0)
+    console.log(n  +  " is even"); // Prints number is even to the console
+    else 
+    console.log(n + ' is odd'); // Prints number is odd
+}
+
+
+console.log('*Activity 2*'); //Gives this section a title
+
+for(let x = 1; x < 11; x++){ //Gives the counter a value
+    console.log((x) + '*5' + '=' +(sum = x * 5)); // Prints the multiplication of 'x' as a multiplication table
+}
+
